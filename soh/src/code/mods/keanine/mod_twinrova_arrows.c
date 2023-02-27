2278b4cea7f65d10a800edd09596fbce399e0ae2
keanine <keanen195@gmail.com>
2023-02-27 17:31:41 +0000

    Improves SFX. Refactored to be clearer

--- soh/src/code/mods/keanine/mod_twinrova_arrows.c
+++ soh/src/code/mods/keanine/mod_twinrova_arrows.c
@@ -1,28 +1,53 @@
 #include "z_boss_tw.h";
 
-void CheckArrowCollision(BossTw* this);
+static int16_t KOTAKE = 0x00;
+static int16_t KOUME = 0x01;
 
-void CheckArrowCollision(BossTw* this, BossTw* sKoumePtr, BossTw* sKotakePtr) {
+void TwinrovaArrows_CheckArrowCollision(BossTw* this, BossTw* sKoumePtr, BossTw* sKotakePtr);
+static void TwinrovaArrows_HitCorrect(BossTw* this);
+static void TwinrovaArrows_HitIncorrect(BossTw* this);
+
+void TwinrovaArrows_CheckArrowCollision(BossTw* this, BossTw* sKoumePtr, BossTw* sKotakePtr) {
     ColliderInfo* info = this->collider.info.acHitInfo;
-    if (this->actor.params == TW_KOTAKE) {
+
+    if (this->actor.params == KOTAKE) {
         if (info->toucher.dmgFlags & DMG_ARROW_FIRE) {
             osSyncPrintf("[MOD] KOTAKE HIT WITH FIRE ARROW");
-            if (this->actor.colChkInfo.health < 2) {
-                this->actor.colChkInfo.health = 2;
-            }
-            Audio_PlayActorSound2(&this->actor, NA_SE_EN_TWINROBA_DAMAGE_VOICE);
+            TwinrovaArrows_HitCorrect(this);
+
+        } else if (info->toucher.dmgFlags & DMG_ARROW_ICE) {
+            TwinrovaArrows_HitIncorrect(this);
         }
-    } else if (this->actor.params == TW_KOUME) {
+
+    } else if (this->actor.params == KOUME) {
         if (info->toucher.dmgFlags & DMG_ARROW_ICE) {
             osSyncPrintf("[MOD] KOTAKE HIT WITH ICE ARROW");
-            if (this->actor.colChkInfo.health < 2) {
-                this->actor.colChkInfo.health = 2;
-            }
-            Audio_PlayActorSound2(&this->actor, NA_SE_EN_TWINROBA_DAMAGE_VOICE);
+            TwinrovaArrows_HitCorrect(this);
+
+        } else if (info->toucher.dmgFlags & DMG_ARROW_FIRE) {
+            TwinrovaArrows_HitIncorrect(this);
         }
     }
 
-    if ((sKoumePtr->actor.colChkInfo.health + sKotakePtr->actor.colChkInfo.health) >= 4) {
-        // BossTw_SetupWait(this, play);
+    //if ((sKoumePtr->actor.colChkInfo.health + sKotakePtr->actor.colChkInfo.health) >= 4) {
+    //    // BossTw_SetupWait(this, play);
+    //}
+}
+
+static void TwinrovaArrows_HitCorrect(BossTw* this) {
+    osSyncPrintf("[MOD] KOTAKE HIT WITH FIRE ARROW");
+    if (this->actor.colChkInfo.health < 2) {
+        this->actor.colChkInfo.health = 2;
+    }
+    Audio_PlayActorSound2(&this->actor, NA_SE_EV_HIT_SOUND);
+    Audio_PlayActorSound2(&this->actor, NA_SE_EN_TWINROBA_DAMAGE_VOICE);
+}
+
+static void TwinrovaArrows_HitIncorrect(BossTw* this) {
+    Audio_PlayActorSound2(&this->actor, NA_SE_IT_SHIELD_REFLECT_SW);
+    if (this->actor.params == KOTAKE) {
+        Audio_PlayActorSound2(&this->actor, NA_SE_EN_TWINROBA_LAUGH);
+    } else if (this->actor.params == KOUME) {
+        Audio_PlayActorSound2(&this->actor, NA_SE_EN_TWINROBA_LAUGH2);
     }
 }
